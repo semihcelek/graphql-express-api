4b3ecfe70e375a492c06e3ab1b3dd60647558604
Semih Celek <cybermoon1999@gmail.com>
2021-03-05 00:49:26 +0300

    Add type definitions for User, Post, Query and Mutations

---
+++ src/types.ts
@@ -0,0 +1,41 @@
+import { gql } from "apollo-server-express";
+
+// createdAt && updatedAt columns should be Date type, instead of String.
+export const typeDefs = gql`
+  scalar Date
+
+  type User {
+    firstName: String!
+    lastName: String
+    passwordHash: String!
+    email: String!
+    createdAt: String!
+    updatedAt: String!
+    id: ID!
+    post: [Post!]
+  }
+  type Post {
+    title: String
+    content: String!
+    createdAt: String!
+    updatedAt: String
+    id: ID!
+    authorId: User
+  }
+
+  type Query {
+    allUsers: [User!]!
+    findUser(id: ID!): User
+    allPosts: [Post!]!
+    findPost(id: ID!): Post
+  }
+  type Mutation {
+    createUser(
+      firstName: String!
+      lastName: String
+      passwordHash: String!
+      email: String!
+    ): User
+    createPost(title: String, content: String!): Post
+  }
+`;
